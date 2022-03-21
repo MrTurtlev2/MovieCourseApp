4ddc2e288aa912580ba8cb60b4322dbf405a94b9
MrTurtlev2 <m.wojnar@pleodigital.com>
2022-03-21 16:56:32 +0100

    fetch data fix

--- src/components/screens/CurrencyScreen.js
+++ src/components/screens/CurrencyScreen.js
@@ -1,4 +1,3 @@
-//import liraries
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {View, Text, StyleSheet, FlatList} from 'react-native';
@@ -7,41 +6,25 @@ import {View, Text, StyleSheet, FlatList} from 'react-native';
 const CurrencyScreen = () => {
     const [currencyValue, setCurrencyValue] = useState(null);
 
-    const API = 'http://api.nbp.pl/api/exchangerates/tables/C/';
     const appi = 'http://api.nbp.pl/api/cenyzlota';
-    const aaa =
-        'https://api.themoviedb.org/3/movie/popular?api_key=ff50baa59a96c64b8d7de41b804d51f0';
 
     const getPlnToForeign = async () => {
-        // const resp = await axios.get(appi);
-        // return resp.data.results;
-
         axios.get(appi).then(response => {
-            console.log(response.data);
-            // setCurrencyValue(response.data);
+            // console.log(response.data);
+            setCurrencyValue(response.data[0].cena);
             // return response.data;
         });
     };
 
     useEffect(() => {
-        // getPlnToForeign()
-        //     .then(data => {
-        //         setCurrencyValue(data?.cena);
-        //         console.log(data);
-        //     })
-        //     .catch(err => {
-        //         console.log(err);
-        //     });
-        getPlnToForeign().then(data => {
-            console.log(data);
-            // setCurrencyValue(data);
-        });
-        // getPlnToForeign();
+        getPlnToForeign();
     }, []);
-    // console.log(currencyValue);
+
+    console.log(currencyValue);
+
     return (
         <View style={styles.container}>
-            {/* <Text>{toString(currencyValue)}</Text> */}
+            <Text>{currencyValue}</Text>
             <Text>grgerg</Text>
         </View>
     );
